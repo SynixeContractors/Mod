2b34df5bf46d2c5ce5c946c39967bc5d71d69f19
BrettMayson <brett@mayson.io>
2023-11-12 12:28:49 -0600

    Update config.cpp

--- addons/radio/config.cpp
+++ addons/radio/config.cpp
@@ -15,14 +15,14 @@ class CfgRadioStations {
     delete ClassicRock109;
     delete PulseEDM;
     delete LifeIreland;
-    //class synixe_radio1 {
-        //name = "Synixe Radio 1 - It's the #1!";
-        //url = "https://radio.synixe.contractors/listen/radio1/radio.mp3";
-    //};
-    //class synixe_radio2 {
-        //name = "Synixe Radio 2 - Rock 'n Soul";
-        //url = "https://radio.synixe.contractors/listen/radio2/radio.mp3";
-    //};
+    // class synixe_radio1 {
+    //     name = "Synixe Radio 1 - It's the #1!";
+    //     url = "https://radio.synixe.contractors/listen/radio1/radio.mp3";
+    // };
+    // class synixe_radio2 {
+    //     name = "Synixe Radio 2 - Rock 'n Soul";
+    //     url = "https://radio.synixe.contractors/listen/radio2/radio.mp3";
+    // };
     class synixe_radio3 {
         name = "Synixe Radio 3 - Spanish Pop";
         url = "https://radio.synixe.contractors/listen/radio3/radio.mp3";
