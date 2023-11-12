34cb0c009703f15f3bae335e68074c1afa98daf5
Matías Di Palma <92798766+matidp4@users.noreply.github.com>
2023-11-12 15:24:27 -0300

    Update config.cpp

--- addons/radio/config.cpp
+++ addons/radio/config.cpp
@@ -12,6 +12,14 @@ class CfgPatches {
 };
 
 class CfgRadioStations {
+    //class synixe_radio1 {
+        //name = "Synixe Radio 1 - It's the #1!";
+        //url = "https://radio.synixe.contractors/listen/radio1/radio.mp3";
+    //};
+    //class synixe_radio2 {
+        //name = "Synixe Radio 2 - Rock 'n Soul";
+        //url = "https://radio.synixe.contractors/listen/radio2/radio.mp3";
+    //};
     class synixe_radio3 {
         name = "Synixe Radio 3 - Spanish Pop";
         url = "https://radio.synixe.contractors/listen/radio3/radio.mp3";
