894313d4a38c496e5b31772b710153b04beb64e4
Amenbreaker <83235712+Amenbreaker@users.noreply.github.com>
2023-12-09 00:04:43 -0300

    Update config.cpp

--- addons/radio/config.cpp
+++ addons/radio/config.cpp
@@ -15,14 +15,14 @@ class CfgRadioStations {
     delete ClassicRock109;
     delete PulseEDM;
     delete LifeIreland;
-    // class synixe_radio1 {
-    //     name = "Synixe Radio 1 - It's the #1!";
-    //     url = "https://radio.synixe.contractors/listen/radio1/radio.mp3";
-    // };
-    // class synixe_radio2 {
-    //     name = "Synixe Radio 2 - Rock 'n Soul";
-    //     url = "https://radio.synixe.contractors/listen/radio2/radio.mp3";
-    // };
+     class synixe_radio1 {
+         name = "Synixe Radio 1 - It's the #1!";
+         url = "https://radio.synixe.contractors/listen/radio1/radio.mp3";
+     };
+     class synixe_radio2 {
+         name = "Synixe Radio 2 - Rock 'n Soul";
+         url = "https://radio.synixe.contractors/listen/radio2/radio.mp3";
+     };
     class synixe_radio3 {
         name = "Synixe Radio 3 - Spanish Pop";
         url = "https://radio.synixe.contractors/listen/radio3/radio.mp3";
@@ -39,20 +39,20 @@ class CfgRadioStations {
         name = "Synixe Radio 6 - Ritmo Pa Tu Cuerpo";
         url = "https://radio.synixe.contractors/listen/radio6/radio.mp3";
     };
-    //class synixe_radio7 {
-    //    name = "Synixe Radio 7 - Alternative Waves";
-    //    url = "https://radio.synixe.contractors/listen/radio7/radio.mp3";
-    //};
-    //class synixe_radio9 {
-    //    name = "Synixe Radio 9 - All Things Math Rock";
-    //    url = "https://radio.synixe.contractors/listen/radio9/radio.mp3";
-    //};
-    //class synixe_radio10 {
-    //    name = "Synixe Radio 10 - Alternative Hiphop 90s";
-    //    url = "https://radio.synixe.contractors/listen/radio10/radio.mp3";
-    //};
-    //class synixe_radio11 {
-    //    name = "Synixe Radio 11 - Beautiful World";
-    //    url = "https://radio.synixe.contractors/listen/radio11/radio.mp3";
-    //};
+    class synixe_radio7 {
+        name = "Synixe Radio 7 - Alternative Waves";
+        url = "https://radio.synixe.contractors/listen/radio7/radio.mp3";
+    };
+    class synixe_radio9 {
+        name = "Synixe Radio 9 - All Things Math Rock";
+        url = "https://radio.synixe.contractors/listen/radio9/radio.mp3";
+    };
+    class synixe_radio10 {
+        name = "Synixe Radio 10 - Alternative Hiphop 90s";
+        url = "https://radio.synixe.contractors/listen/radio10/radio.mp3";
+    };
+    class synixe_radio11 {
+        name = "Synixe Radio 11 - Beautiful World";
+        url = "https://radio.synixe.contractors/listen/radio11/radio.mp3";
+    };
 };
