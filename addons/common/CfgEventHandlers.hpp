f812ce7c7cc87d820cacd6e264c3eefd1a98d9df
BrettMayson <brett@mayson.io>
2024-01-13 21:01:44 -0600

    fix postinit missing

--- addons/common/CfgEventHandlers.hpp
+++ addons/common/CfgEventHandlers.hpp
@@ -14,6 +14,5 @@ class Extended_PostInit_EventHandlers {
     class ADDON {
         clientInit = QUOTE(call COMPILE_FILE(XEH_postInitClient));
         serverInit = QUOTE(call COMPILE_FILE(XEH_postInitServer));
-        init = QUOTE(call COMPILE_FILE(XEH_postInit));
     };
 };
