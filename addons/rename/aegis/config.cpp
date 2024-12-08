8d1b95ec785290f94cdca9f93bb004d3763c62ef
BrettMayson <brett@mayson.io>
2024-12-07 19:34:02 -0600

    rename aegis vehicles

--- addons/rename/aegis/config.cpp
+++ addons/rename/aegis/config.cpp
@@ -10,7 +10,14 @@ class CfgPatches {
             "synixe_main",
             "ace_compat_aegis_realisticnames",
             "A3_Aegis_Weapons_F_Aegis",
-            "A3_Atlas_Weapons_F_Atlas"
+            "A3_Atlas_Weapons_F_Atlas",
+            // Strider
+            "A3_Atlas_Soft_F_Atlas_MRAP_03",
+            // Marshall
+            "A3_Aegis_Armor_F_Aegis_APC_Wheeled_01",
+            "Vehicles_F_lxWS_APC_Wheeled_01",
+            // Ram 1500
+            "A3_Aegis_Soft_F_Aegis_RF_Pickup_01",
         };
         skipWhenMissingDependencies = 1;
         VERSION_CONFIG;
@@ -18,3 +25,4 @@ class CfgPatches {
 };
 
 #include "CfgWeapons.hpp"
+#include "CfgVehicles.hpp"
