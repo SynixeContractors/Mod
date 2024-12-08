e9d7c5c86582b31f82b2d6cedaa865e71cc7f1d6
BrettMayson <brett@mayson.io>
2024-12-07 20:50:24 -0600

    mgp hide and rename

---
+++ addons/mgp_hide/CfgWeapons.hpp
@@ -0,0 +1,11 @@
+class CfgWeapons {
+    #define HIDE(CLASS) class CLASS: ItemCore { scope = 1; scopeArsenal = 1; scopeCurator = 1; }
+    #define RENAME(CLASS,DISPLAY) class CLASS: ItemCore { displayName = DISPLAY; }
+
+    class ItemCore;
+
+    #include "HideBelts.hpp"
+    #include "HideHats.hpp"
+    #include "HideHelmets.hpp"
+    #include "HideVests.hpp"
+};
