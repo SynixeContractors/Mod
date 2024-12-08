a5e0b38f45c5d90d3a61aba5899122c6e7d18b74
BrettMayson <brett@mayson.io>
2024-12-07 21:47:04 -0600

    medic helmets

---
+++ addons/mgp_medic/config.cpp
@@ -0,0 +1,17 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"synixe_main", "milgp_headgear_cfg"};
+        skipWhenMissingDependencies = 1;
+        author = ECSTRING(main,Author);
+        authors[] = {"Brett"};
+        url = ECSTRING(main,URL);
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgWeapons.hpp"
