6f057eb07bb7f93425ba69c407913d611c51b3c6
Brett Mayson <brett@mayson.io>
2025-10-20 04:08:52 -0600

    re-add medic helmets for now

---
+++ addons/mgp_medic/CfgWeapons.hpp
@@ -0,0 +1,203 @@
+class CfgWeapons {
+    // RGR Medic Airframe
+    class milgp_h_airframe_01_blk_rgr_whtc;
+    class CLASS(h_airframe_01_blk_rgr_medicc): milgp_h_airframe_01_blk_rgr_whtc {
+        author = "Brett";
+        displayName = "Airframe (BLK/RGR/Medic Cover)";
+        picture = QPATHTOF(ui\h_airframe_01_blk_rgr_medicc.paa);
+        hiddenSelectionsTextures[] = {
+            QPATHTOF(textures\h_airframe_blk_medicc_co.paa),
+            "\milgp_headgear\textures\h_opscore_blk_whtc_co.paa",
+            "\milgp_headgear\textures\h_gear_01_rgr_co.paa"
+        };
+    };
+    class milgp_h_airframe_01_khk_rgr_whtc;
+    class CLASS(h_airframe_01_khk_rgr_medicc): milgp_h_airframe_01_khk_rgr_whtc {
+        author = "Brett";
+        displayName = "Airframe (KHK/RGR/Medic Cover)";
+        picture = QPATHTOF(ui\h_airframe_01_khk_rgr_medicc.paa);
+        hiddenSelectionsTextures[] = {
+            QPATHTOF(textures\h_airframe_khk_medicc_co.paa),
+            "\milgp_headgear\textures\h_opscore_khk_whtc_co.paa",
+            "\milgp_headgear\textures\h_gear_01_rgr_co.paa"
+        };
+    };
+    class milgp_h_airframe_01_rgr_rgr_whtc;
+    class CLASS(h_airframe_01_rgr_rgr_medicc): milgp_h_airframe_01_rgr_rgr_whtc {
+        author = "Brett";
+        displayName = "Airframe (RGR/RGR/Medic Cover)";
+        picture = QPATHTOF(ui\h_airframe_01_rgr_rgr_medicc.paa);
+        hiddenSelectionsTextures[] = {
+            QPATHTOF(textures\h_airframe_rgr_medicc_co.paa),
+            "\milgp_headgear\textures\h_opscore_khk_whtc_co.paa",
+            "\milgp_headgear\textures\h_gear_01_rgr_co.paa"
+        };
+    };
+    
+
+    // KHK Medic Airframe
+    class milgp_h_airframe_01_blk_khk_whtc;
+    class CLASS(h_airframe_01_blk_khk_medicc): milgp_h_airframe_01_blk_khk_whtc {
+        author = "Brett";
+        displayName = "Airframe (BLK/KHK/Medic Cover)";
+        picture = QPATHTOF(ui\h_airframe_01_blk_khk_medicc.paa);
+        hiddenSelectionsTextures[] = {
+            QPATHTOF(textures\h_airframe_blk_medicc_co.paa),
+            "\milgp_headgear\textures\h_opscore_blk_whtc_co.paa",
+            "\milgp_headgear\textures\h_gear_01_khk_co.paa"
+        };
+    };
+    class milgp_h_airframe_01_khk_khk_whtc;
+    class CLASS(h_airframe_01_khk_khk_medicc): milgp_h_airframe_01_khk_khk_whtc {
+        author = "Brett";
+        displayName = "Airframe (KHK/KHK/Medic Cover)";
+        picture = QPATHTOF(ui\h_airframe_01_khk_khk_medicc.paa);
+        hiddenSelectionsTextures[] = {
+            QPATHTOF(textures\h_airframe_khk_medicc_co.paa),
+            "\milgp_headgear\textures\h_opscore_khk_whtc_co.paa",
+            "\milgp_headgear\textures\h_gear_01_khk_co.paa"
+        };
+    };
+    class milgp_h_airframe_01_rgr_khk_whtc;
+    class CLASS(h_airframe_01_rgr_khk_medicc): milgp_h_airframe_01_rgr_khk_whtc {
+        author = "Brett";
+        displayName = "Airframe (RGR/KHK/Medic Cover)";
+        picture = QPATHTOF(ui\h_airframe_01_rgr_khk_medicc.paa);
+        hiddenSelectionsTextures[] = {
+            QPATHTOF(textures\h_airframe_rgr_medicc_co.paa),
+            "\milgp_headgear\textures\h_opscore_khk_whtc_co.paa",
+            "\milgp_headgear\textures\h_gear_01_khk_co.paa"
+        };
+    };
+
+    // BLK Medic Airframe
+    class milgp_h_airframe_01_blk_blk_whtc;
+    class CLASS(h_airframe_01_blk_blk_medicc): milgp_h_airframe_01_blk_blk_whtc {
+        author = "Brett";
+        displayName = "Airframe (BLK/BLK/Medic Cover)";
+        picture = QPATHTOF(ui\h_airframe_01_blk_blk_medicc.paa);
+        hiddenSelectionsTextures[] = {
+            QPATHTOF(textures\h_airframe_blk_medicc_co.paa),
+            "\milgp_headgear\textures\h_opscore_blk_whtc_co.paa",
+            "\milgp_headgear\textures\h_gear_01_blk_co.paa"
+        };
+    };
+    class milgp_h_airframe_01_khk_blk_whtc;
+    class CLASS(h_airframe_01_khk_blk_medicc): milgp_h_airframe_01_khk_blk_whtc {
+        author = "Brett";
+        displayName = "Airframe (KHK/BLK/Medic Cover)";
+        picture = QPATHTOF(ui\h_airframe_01_khk_blk_medicc.paa);
+        hiddenSelectionsTextures[] = {
+            QPATHTOF(textures\h_airframe_khk_medicc_co.paa),
+            "\milgp_headgear\textures\h_opscore_khk_whtc_co.paa",
+            "\milgp_headgear\textures\h_gear_01_blk_co.paa"
+        };
+    };
+    class milgp_h_airframe_01_rgr_blk_whtc;
+    class CLASS(h_airframe_01_rgr_blk_medicc): milgp_h_airframe_01_rgr_blk_whtc {
+        author = "Brett";
+        displayName = "Airframe (RGR/BLK/Medic Cover)";
+        picture = QPATHTOF(ui\h_airframe_01_rgr_blk_medicc.paa);
+        hiddenSelectionsTextures[] = {
+            QPATHTOF(textures\h_airframe_rgr_medicc_co.paa),
+            "\milgp_headgear\textures\h_opscore_khk_whtc_co.paa",
+            "\milgp_headgear\textures\h_gear_01_blk_co.paa"
+        };
+    };
+
+    // BLK Medic Opscore
+    class milgp_h_opscore_01_rgr_blk_whtc;
+    class CLASS(h_opscore_01_rgr_blk_medicc): milgp_h_opscore_01_rgr_blk_whtc {
+        author = "Brett";
+        displayName = "OpsCore (RGR/BLK/Medic Cover)";
+        picture = QPATHTOF(ui\h_opscore_01_rgr_blk_medicc.paa);
+        hiddenSelectionsTextures[] = {
+            QPATHTOF(textures\h_opscore_blk_medicc_co.paa),
+            "\milgp_headgear\textures\h_gear_01_blk_co.paa"
+        };
+    };
+    class milgp_h_opscore_01_khk_blk_whtc;
+    class CLASS(h_opscore_01_khk_blk_medicc): milgp_h_opscore_01_khk_blk_whtc {
+        author = "Brett";
+        displayName = "OpsCore (KHK/BLK/Medic Cover)";
+        picture = QPATHTOF(ui\h_opscore_01_khk_blk_medicc.paa);
+        hiddenSelectionsTextures[] = {
+            QPATHTOF(textures\h_opscore_blk_medicc_co.paa),
+            "\milgp_headgear\textures\h_gear_01_blk_co.paa"
+        };
+    };
+    class milgp_h_opscore_01_blk_blk_whtc;
+    class CLASS(h_opscore_01_blk_blk_medicc): milgp_h_opscore_01_blk_blk_whtc {
+        author = "Brett";
+        displayName = "OpsCore (BLK/BLK/Medic Cover)";
+        picture = QPATHTOF(ui\h_opscore_01_blk_blk_medicc.paa);
+        hiddenSelectionsTextures[] = {
+            QPATHTOF(textures\h_opscore_blk_medicc_co.paa),
+            "\milgp_headgear\textures\h_gear_01_blk_co.paa"
+        };
+    };
+
+    // KHK Medic Opscore
+    class milgp_h_opscore_01_rgr_khk_whtc;
+    class CLASS(h_opscore_01_rgr_khk_medicc): milgp_h_opscore_01_rgr_khk_whtc {
+        author = "Brett";
+        displayName = "OpsCore (RGR/KHK/Medic Cover)";
+        picture = QPATHTOF(ui\h_opscore_01_rgr_khk_medicc.paa);
+        hiddenSelectionsTextures[] = {
+            QPATHTOF(textures\h_opscore_rgr_medicc_co.paa),
+            "\milgp_headgear\textures\h_gear_01_khk_co.paa"
+        };
+    };
+    class milgp_h_opscore_01_khk_khk_whtc;
+    class CLASS(h_opscore_01_khk_khk_medicc): milgp_h_opscore_01_khk_khk_whtc {
+        author = "Brett";
+        displayName = "OpsCore (KHK/KHK/Medic Cover)";
+        picture = QPATHTOF(ui\h_opscore_01_khk_khk_medicc.paa);
+        hiddenSelectionsTextures[] = {
+            QPATHTOF(textures\h_opscore_khk_medicc_co.paa),
+            "\milgp_headgear\textures\h_gear_01_khk_co.paa"
+        };
+    };
+    class milgp_h_opscore_01_blk_khk_whtc;
+    class CLASS(h_opscore_01_blk_khk_medicc): milgp_h_opscore_01_blk_khk_whtc {
+        author = "Brett";
+        displayName = "OpsCore (BLK/KHK/Medic Cover)";
+        picture = QPATHTOF(ui\h_opscore_01_blk_khk_medicc.paa);
+        hiddenSelectionsTextures[] = {
+            QPATHTOF(textures\h_opscore_blk_medicc_co.paa),
+            "\milgp_headgear\textures\h_gear_01_khk_co.paa"
+        };
+    };
+
+    // RGR Medic Opscore
+    class milgp_h_opscore_01_rgr_rgr_whtc;
+    class CLASS(h_opscore_01_rgr_rgr_medicc): milgp_h_opscore_01_rgr_rgr_whtc {
+        author = "Brett";
+        displayName = "OpsCore (RGR/RGR/Medic Cover)";
+        picture = QPATHTOF(ui\h_opscore_01_rgr_rgr_medicc.paa);
+        hiddenSelectionsTextures[] = {
+            QPATHTOF(textures\h_opscore_rgr_medicc_co.paa),
+            "\milgp_headgear\textures\h_gear_01_rgr_co.paa"
+        };
+    };
+    class milgp_h_opscore_01_khk_rgr_whtc;
+    class CLASS(h_opscore_01_khk_rgr_medicc): milgp_h_opscore_01_khk_rgr_whtc {
+        author = "Brett";
+        displayName = "OpsCore (KHK/RGR/Medic Cover)";
+        picture = QPATHTOF(ui\h_opscore_01_khk_rgr_medicc.paa);
+        hiddenSelectionsTextures[] = {
+            QPATHTOF(textures\h_opscore_khk_medicc_co.paa),
+            "\milgp_headgear\textures\h_gear_01_rgr_co.paa"
+        };
+    };
+    class milgp_h_opscore_01_blk_rgr_whtc;
+    class CLASS(h_opscore_01_blk_rgr_medicc): milgp_h_opscore_01_blk_rgr_whtc {
+        author = "Brett";
+        displayName = "OpsCore (BLK/RGR/Medic Cover)";
+        picture = QPATHTOF(ui\h_opscore_01_blk_rgr_medicc.paa);
+        hiddenSelectionsTextures[] = {
+            QPATHTOF(textures\h_opscore_blk_medicc_co.paa),
+            "\milgp_headgear\textures\h_gear_01_rgr_co.paa"
+        };
+    };
+};
---
+++ addons/mgp_medic/script_component.hpp
@@ -0,0 +1,4 @@
+#define COMPONENT mgp_medic
+#define COMPONENT_BEAUTIFIED MGP Medic
+#include "..\main\script_mod.hpp"
+#include "..\main\script_macros.hpp"
