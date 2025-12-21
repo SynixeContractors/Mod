7b5485958cd951f08ad0eccc79160a7de60e3d30
Brett Mayson <brett@mayson.io>
2025-12-21 00:09:58 -0600

    remove GL, update tag

--- addons/main/config.cpp
+++ addons/main/config.cpp
@@ -15,8 +15,8 @@ class ACE_Tags {
     class GVAR(logo) {
         displayName = "Synixe Logo";
         requiredItem = "ACE_SpraypaintYellow";
-        textures[] = {QPATHTOF(ui\synixe_ca.paa)};
-        icon = QPATHTOF(ui\synixe_ca.paa);
+        textures[] = {QPATHTOF(tag_ca.paa)};
+        icon = QPATHTOF(tag_ca.paa);
     };
 };
 
@@ -27,16 +27,6 @@ class CfgEditorCategories {
         scope = 2;
         curatorScope = 2;
         displayName = "Synixe Contractors";
-    };
-};
-
-// GL Bounce
-
-class CfgAmmo {
-    class SmokeShell;
-    class G_40mm_Smoke: SmokeShell {
-        simulation = "shotSmoke";
-        deflectionSlowDown = 0.1;
     };
 };
 
