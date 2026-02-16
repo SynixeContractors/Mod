296b388abfc464e9579e4072154ba47f8dd0b3de
Brett Mayson <brett@mayson.io>
2026-02-16 10:06:28 -0600

    broken radio models

--- addons/acre/CfgWeapons.hpp
+++ addons/acre/CfgWeapons.hpp
@@ -5,6 +5,7 @@ class CfgWeapons {
         scope = 1;
         author = "Mighty";
         picture = "\idi\acre\addons\sys_prc343\Data\prc343_icon.paa";
+        model = "\idi\acre\addons\sys_prc343\Data\models\acre_prc343_model.p3d";
         displayName = "AN/PRC-343 (Destroyed)";
         descriptionShort = "AN/PRC-343 PRR Radio";
         descriptionUse = "";
@@ -16,6 +17,7 @@ class CfgWeapons {
         scope = 1;
         author = "Mighty";
         picture = "\idi\acre\addons\sys_prc148\Data\prc148_icon.paa";
+        model = "\idi\acre\addons\sys_prc148\Data\models\prc148.p3d";
         displayName = "AN/PRC-148 (Destroyed)";
         descriptionShort = "AN/PRC-148 Radio";
         descriptionUse = "";
@@ -27,6 +29,7 @@ class CfgWeapons {
         scope = 1;
         author = "Mighty";
         picture = "\idi\acre\addons\sys_prc152\Data\PRC152c_ico.paa";
+        model = "\idi\acre\addons\sys_prc152\Data\Models\PRC152.p3d";
         displayName = "AN/PRC-152 (Destroyed)";
         descriptionShort = "AN/PRC-152 VHF/UHF Radio";
         descriptionUse = "";
@@ -50,6 +53,7 @@ class CfgWeapons {
         scope = 1;
         author = "Mighty";
         picture = "\idi\acre\addons\sys_prc77\Data\prc77_icon.paa";
+        model = "\idi\acre\addons\sys_prc77\Data\models\prc_77.p3d";
         displayName = "AN/PRC-77 (Destroyed)";
         descriptionShort = "AN/PRC-77 Manpack Radio";
         descriptionUse = "";
@@ -62,6 +66,7 @@ class CfgWeapons {
         scope = 1;
         author = "Mighty";
         picture = "\idi\acre\addons\sys_sem52sl\data\ui\sem52sl_icon.paa";
+        model = "\idi\acre\addons\sys_sem52sl\Data\model\sem52sl.p3d";
         displayName = "SEM 52 SL (Destroyed)";
         descriptionShort = "Sender/Empfänger, mobil SEM 52 SL";
         descriptionUse = "";
@@ -73,6 +78,7 @@ class CfgWeapons {
         scope = 1;
         author = "Mighty";
         picture = "\idi\acre\addons\sys_sem70\data\ui\sem70_icon.paa";
+        model = "\idi\acre\addons\sys_sem70\data\model\sem70.p3d";
         displayName = "SEM 70 (Destroyed)";
         descriptionShort = "Sender/Empfänger, mobil SEM 70";
         descriptionUse = "";
@@ -85,6 +91,7 @@ class CfgWeapons {
         scope = 1;
         author = "Mighty";
         picture = "\idi\acre\addons\sys_bf888s\Data\static\bf888s_icon.paa";
+        model = "\idi\acre\addons\sys_bf888s\Data\models\acre_bf888s_model.p3d";
         displayName = "Beofeng 888S (Destroyed)";
         descriptionShort = "Beofeng BF-888S UHF Radio";
         descriptionUse = "";
