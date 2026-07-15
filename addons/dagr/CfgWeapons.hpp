0c1ba45f7faf1f8fd96d7aaa935773a904ed3a4d
Brett Mayson <brett@mayson.io>
2026-07-15 05:31:28 -0600

    dagr isn't uav

--- addons/dagr/CfgWeapons.hpp
+++ addons/dagr/CfgWeapons.hpp
@@ -1,9 +1,6 @@
 class CfgWeapons {
     class ACE_ItemCore;
-    class CBA_MiscItem_ItemInfo;
     class ACE_DAGR: ACE_ItemCore {
-        class ItemInfo: CBA_MiscItem_ItemInfo {
-            type = 621;
-        };
+        simulation = "ItemGPS";
     };
 };
